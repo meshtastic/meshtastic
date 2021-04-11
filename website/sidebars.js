ee2e7e61a7db2e2cf5d8a2f678f86d53cfcd9ab2
Sacha Weatherstone <sachaw100@hotmail.com>
2021-04-11 15:49:24 +1000

    Start docs restructure

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,6 +1,19 @@
 module.exports = {
   Sidebar: {
-    "Getting Started": ["overview"],
+    "Getting Started": [
+      "getting_started/overview_getting_started",
+      "getting_started/flashing_firmware_getting_started",
+      "getting_started/concepts_getting_started",
+      {
+        "Howto's": [],
+      },
+    ],
+    Hardware: [
+      "hardware/supported_hardware_hardware",
+      "hardware/behaviour_hardware",
+      "hardware/antenna_hardware",
+    ],
+
     Device: [
       "device/radio-settings",
       "device/supported-hardware",
@@ -9,33 +22,56 @@ module.exports = {
       "device/documents",
     ],
     Software: [
-      "software/sw-design",
-      "software/remote-hardware-service",
-      "software/rak815",
-      "software/power",
-      "software/plugin-api",
-      "software/pinetab",
-      "software/nrf52-TODO",
-      "software/mqtt",
-      "software/mesh-alg",
-      "software/install-OSX",
-      "software/esp32-arduino-build-notes",
-      "software/device-api",
-      "software/crypto",
-      "software/build-instructions",
-      "software/ant",
-    ],
-    "Meshtastic.js": [
-      "js/getting-started",
-      "js/connecting",
-      "js/events",
-      "js/http-api",
+      "software/overview-software",
       {
-        type: "link",
-        label: "API Docs",
-        href: "https://js.meshtastic.org",
+        "Meshtastic Android": [],
+      },
+      {
+        "Meshtastic.js": [
+          "js/getting-started",
+          "js/connecting",
+          "js/events",
+          "js/http-api",
+          {
+            type: "link",
+            label: "API Docs",
+            href: "https://js.meshtastic.org",
+          },
+        ],
+      },
+      {
+        "Meshtastic-python": [
+          {
+            type: "link",
+            label: "API Docs",
+            href: "https://meshtastic.github.io/Meshtastic-python",
+          },
+        ],
+      },
+      {
+        Other: [
+          "software/other/sw-design",
+          "software/other/remote-hardware-service",
+          "software/other/rak815",
+          "software/other/power",
+          "software/other/plugin-api",
+          "software/other/pinetab",
+          "software/other/nrf52-TODO",
+          "software/other/mqtt",
+          "software/other/mesh-alg",
+          "software/other/install-OSX",
+          "software/other/esp32-arduino-build-notes",
+          "software/other/device-api",
+          "software/other/crypto",
+          "software/other/build-instructions",
+          "software/other/ant",
+        ],
       },
     ],
-    Protobufs: ["protobufs/api"],
+    Developers: [
+      {
+        Protobufs: ["protobufs/api"],
+      },
+    ],
   },
 };
