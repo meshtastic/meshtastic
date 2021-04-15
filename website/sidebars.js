4c9489e4b32e9ab4d972ff2ff459d7c8ac75c88c
Foster Irwin <foster@jfirwin.com>
2021-04-15 13:59:43 -0600

    Created OTA firmware flashing page. TODO create doc.

--- website/sidebars.js
+++ website/sidebars.js
@@ -7,10 +7,10 @@ module.exports = {
         "Flashing Firmware": [
           "getting_started/flashing_firmware/flashing_firmware_cli",
           "getting_started/flashing_firmware/flashing_firmware_gui",
-          "getting_started/flashing_firmware/flashing_firmware_ota",
           "getting_started/flashing_firmware/overview",
           "getting_started/flashing_firmware/linux",
           "getting_started/flashing_firmware/macos",
+          "getting_started/flashing_firmware/ota",
           "getting_started/flashing_firmware/windows",
         ],
         "Howto's": [],
