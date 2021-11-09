ee7cba3e191dfdcab5679189111a1588340b1826
KrazyKerbalnaut <spenci.one.kanobi@gmail.com>
2021-11-09 15:46:42 -0700

    Change verbage on banner regarding android app to point to installation method.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
     announcementBar: {
       id: 'play_store_workaround',
       content:
-        '<a href="/docs/software/android/android-installation" href="#">Android app unavailable in the Google Play Store store. Install by side-loading from git-hub or from Amazon Apps.</a>',
+        'Android app unavailable in the Google Play Store store. Install by side-loading from <a href="https://github.com/meshtastic/Meshtastic-Android/releases">GitHub</a> or from <a href="https://www.amazon.com/Geeksville-Industries-Meshtastic/dp/B08CY9394Q">Amazon</a>.',
       backgroundColor: '#ff9999',
       textColor: '#091E42',
       isCloseable: false,
