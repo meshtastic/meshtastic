0c4a61007e6f43c44352c13a368eb8ae581e5937
Foster Irwin <foster@jfirwin.com>
2022-02-04 09:34:27 -0700

    removed createElement and used JSX

--- src/pages/downloads/_components/HeaderText.tsx
+++ src/pages/downloads/_components/HeaderText.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 
 export const HeaderText = ({type, text, link}): JSX.Element => {
 
-  const anchor = React.createElement(
-    'a',
-    {className: "hash-link", href: `#${link}`, title: "Direct link to heading"}
-  )
+  const Header = type;
 
-  const heading = React.createElement(
-    type || 'h1',
-    link ? {className: "anchor anchorWithHideOnScrollNavbar_node_modules-@docusaurus-theme-classic-lib-next-theme-Heading-styles-module"} : null,
-    text, link ? anchor : null
-  )
-
-  return heading
+  return (
+    <Header className="anchor anchorWithHideOnScrollNavbar_node_modules-@docusaurus-theme-classic-lib-next-theme-Heading-styles-module">
+      {text}
+      {link ? <a className="hash-link" href={`#${link}`} title="Direct link to heading"/> : null}
+    </Header>
+  );
 
 };
