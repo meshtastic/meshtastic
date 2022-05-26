94a9c9d5a30bcc5eadac60d6a23f205ec259fdf0
Foster Irwin <foster@jfirwin.com>
2022-05-26 14:32:49 -0600

    initial sponsor page

---
+++ src/pages/sponsor/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+const Sponsor = (): JSX.Element => {
+  return(
+    <Layout
+      title="Sponsor"
+      description="Contribute Funds to Meshtastic"
+    >
+    </Layout>
+  )
+}
+
+export default Sponsor;
