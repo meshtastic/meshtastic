4ba6f3842fad1c0665ea638fbdbe1a822a40dbe0
djscoutmaster <farnsworth.tony@gmail.com>
2022-06-10 19:45:23 -0600

    typo on homepage
    typo on homepage fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ const features = [
       <>
         Create a highly scalable mesh with hardware on a multitude of platforms
         to fit your unique requirements: Create an environment monitoring mesh
-        and produce real-time heatmaps, or maybe decentralizeds, encrypted
+        and produce real-time heatmaps, or maybe decentralized, encrypted
         messaging network, your imagination is the limit.
       </>
     ),
