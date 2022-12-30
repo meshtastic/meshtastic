2bb65d5c79aacec0d63bef0eae0bca82a74bc21b
Foster Irwin <foster@jfirwin.com>
2022-12-30 14:01:56 -0700

    added custom color scheme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,6 +89,19 @@ const config = {
     colorMode: {
       respectPrefersColorScheme: true,
     },
+    mermaid: {
+      theme: {light: 'base', dark: 'base'},
+      options: {
+        themeVariables: {
+          primaryColor: '#67EA94',
+          primaryTextColor: 'var(--tw-prose-headings)',
+          primaryBorderColor: '#4D4D4D',
+          lineColor: '#EAD667',
+          secondaryColor: '#EA67BD',
+          tertiaryColor: '#677CEA'
+        },
+      },
+    }
   },
   plugins: [
     () => {
