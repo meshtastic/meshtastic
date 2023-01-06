4c1140599289c949344eb7404b766d25273e71f7
Foster Irwin <foster@jfirwin.com>
2023-01-06 13:36:52 -0700

    initial TH region

---
+++ src/data/regions/th.ts
@@ -0,0 +1,13 @@
+import { IRegion } from '../region';
+
+export const TH: IRegion = {
+    name: "TH",
+    freqStart: 920.0,
+    freqEnd: 925.0,
+    dutyCycle: 100,
+    spacing: 0,
+    powerLimit: 16,
+    audioPermitted: true,
+    frequencySwitching: false,
+    wideLora: false,
+}
