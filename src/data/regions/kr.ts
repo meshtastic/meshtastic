ec1ad43fed6700e7754c92f6d3a6d75ceb2eb7c1
Foster Irwin <foster@jfirwin.com>
2023-01-06 13:28:51 -0700

    initial KR region

---
+++ src/data/regions/kr.ts
@@ -0,0 +1,13 @@
+import { IRegion } from '../region';
+
+export const KR: IRegion = {
+    name: "KR",
+    freqStart: 920.0,
+    freqEnd: 925.0,
+    dutyCycle: 100,
+    spacing: 0,
+    powerLimit: 0,
+    audioPermitted: true,
+    frequencySwitching: false,
+    wideLora: false,
+}
