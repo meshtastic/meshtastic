e9a0b2cae923df5b46d8a312dd98854f5cd91cc6
Sacha Weatherstone <sachaw100@hotmail.com>
2023-05-14 10:41:39 +1000

    Add Chirpy 404 page

---
+++ src/theme/NotFound.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Translate, { translate } from "@docusaurus/Translate";
+import { PageMetadata } from "@docusaurus/theme-common";
+import Layout from "@theme/Layout";
+export default function NotFound() {
+	return (
+		<>
+			<PageMetadata
+				title={translate({
+					id: "theme.NotFound.title",
+					message: "Page Not Found",
+				})}
+			/>
+			<Layout>
+				<main className="container margin-vert--xl">
+					<div className="row">
+						<div className="col col--6 col--offset-3">
+							<h1 className="hero__title">
+								<Translate
+									id="theme.NotFound.title"
+									description="The title of the 404 page"
+								>
+									404 - Page Not Found
+								</Translate>
+							</h1>
+							<img src="/design/chirpy.png" alt='Chirpy' />
+						</div>
+					</div>
+				</main>
+			</Layout>
+		</>
+	);
+}
