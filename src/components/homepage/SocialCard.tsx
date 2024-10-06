ce56860756159d514efdbd9c98f9101f23645e55
rcarteraz <robert.l.carter2@gmail.com>
2024-10-05 21:30:55 -0700

    Update Social Card Components and Add Mastodon Link with Verification (#1487)
    * updates to social cards and add mastodon link
    
    * revert a few changes not necessary
    
    * rename logo file

--- src/components/homepage/SocialCard.tsx
+++ src/components/homepage/SocialCard.tsx
@@ -1,27 +1,28 @@
 import type React from "react";
-
 import { FiExternalLink } from "react-icons/fi";
 
 export interface SocialCardProps {
   children: React.ReactNode;
   color: string;
   link: string;
+  isMeLink?: boolean;
 }
 
 export const SocialCard = ({
   children,
   color,
   link,
+  isMeLink = false,
 }: SocialCardProps): JSX.Element => {
   return (
     <div
-      className={`group relative flex h-24 w-36 min-w-max flex-shrink-0 rounded-xl shadow-xl ${color} m-2`}
+      className={`group relative flex h-20 w-28 min-w-max flex-shrink-0 rounded-lg shadow-lg ${color} m-1`}
     >
       {children}
       <a
         className="absolute top-0 left-0 right-0 bottom-0 hidden rounded-xl border border-accent bg-secondary bg-opacity-95 text-2xl shadow-xl group-hover:flex"
         href={link}
-        rel="noreferrer"
+        rel={isMeLink ? "me noreferrer" : "noreferrer"}
         target="_blank"
       >
         <FiExternalLink className="m-auto" />
