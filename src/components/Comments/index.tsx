27757aeb65d98a843313f7f3cc1d739ef763c789
rcarteraz <robert.l.carter2@gmail.com>
2024-11-14 14:00:33 -0700

    update docs to latest 3.6.1 and fix comments to work

---
+++ src/components/Comments/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Giscus from "@giscus/react";
+import { useColorMode } from "@docusaurus/theme-common";
+
+export default function Comments(): JSX.Element {
+  const { colorMode } = useColorMode();
+
+  return (
+    <div>
+      <Giscus
+        id="comments"
+        repo="meshtastic/discussions"
+        repoId="R_kgDONDs6gA"
+        category="Blog Post Comments"
+        categoryId="DIC_kwDONDs6gM4Cjy3h"
+        mapping="pathname"
+        strict="1"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme={colorMode}
+        lang="en"
+        loading="lazy"
+        crossorigin="anonymous"
+        async
+      />
+    </div>
+  );
+}
