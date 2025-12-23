04d5e23a04bd4e54c71e283c1517dd0d16c146cb
Dan D <dan.ditomaso@gmail.com>
2025-12-23 14:14:40 -0500

    fix: formatting issues

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,7 +121,7 @@ const config = {
         },
       };
     },
-    "@docusaurus/plugin-vercel-analytics"
+    "@docusaurus/plugin-vercel-analytics",
   ],
   scripts: [
     ...(process.env.COOKIEYES_CLIENT_ID
